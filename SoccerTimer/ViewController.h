8ea2b86ffba644f33c6cae119d89fda5b298e4e9
Russ <rudavis@gmail.com>
2014-01-08 15:46:14 -0500

    Initial Commit

---
+++ SoccerTimer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SoccerTimer
+//
+//  Created by Russ Davis on 1/8/14.
+//  Copyright (c) 2014 rtb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
